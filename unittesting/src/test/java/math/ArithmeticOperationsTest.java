5e52dbff754829968b517f9222837fc0e5005773
Angela Papadopoulou <angelikeepap@gmail.com>
2021-04-29 16:28:31 +0300

    Add more test cases for the divide and multiply methods

--- unittesting/src/test/java/math/ArithmeticOperationsTest.java
+++ unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -1,17 +1,33 @@
 package math;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+
 
 public class ArithmeticOperationsTest {
 	
 	ArithmeticOperations ao = new ArithmeticOperations();
 
+	@Test
+	public void test_divide_normal_int() {
+		Assert.assertEquals(3.0, ao.divide(6.0, 2.0), 0);
+	}
+	
+	@Test
+	public void test_divide_normal_double() {
+		Assert.assertEquals(1.5, ao.divide(3.0, 2.0), 1e-8);
+	}
+	
 	@Test(expected = ArithmeticException.class)
 	public void test_divide_zero() {
 		ao.divide(3, 0);
+	}
+
+	@Test
+	public void test_multiply_normal_double() {
+		Assert.assertEquals(6, ao.multiply(3, 2));
 	}
 	
 	@Rule
@@ -20,19 +36,30 @@ public class ArithmeticOperationsTest {
 	@Test
 	public void test_multiply_firstNumberNegative_RuleException() {
 		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("x & y should be >= 0");
 		ao.multiply(-3, 3);
 	}
 	
 	@Test
 	public void test_multiply_secondNumberNegativenegative_RuleException() {
 		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("x & y should be >= 0");
 		ao.multiply(3, -3);
 	}
 	
 	@Test
 	public void test_multiply_bothNumbersNegativenegative_RuleException() {
 		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("x & y should be >= 0");
 		ao.multiply(-3, -3);
 	}
+	
+	@Test
+	public void test_multiply_bigNumbers_RuleException() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("The product does not fit in an Integer variable");
+		ao.multiply(Integer.MAX_VALUE, 3);
+	}
+
 
 }
