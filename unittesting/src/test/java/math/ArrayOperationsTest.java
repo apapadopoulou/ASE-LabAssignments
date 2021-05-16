98c0e1e3d05b9a0888501696faebf2bf35a8edca
Angela Papadopoulou <angelikeepap@gmail.com>
2021-05-16 18:20:55 +0300

    #10 Add test cases for the ArrayOperations class and corresponding test resources

--- unittesting/src/test/java/math/ArrayOperationsTest.java
+++ unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -12,7 +12,7 @@ public class ArrayOperationsTest {
 	ArrayOperations ao = new ArrayOperations();
 
 	@Test
-	public void test_findPrimesInFile_Mocking() {
+	public void test_findPrimesInFileNormalCase_Mocking() {
 		FileIO fio = mock(FileIO.class);
 		MyMath mm = mock(MyMath.class);
 		
@@ -29,6 +29,79 @@ public class ArrayOperationsTest {
 		when(mm.isPrime(23)).thenReturn(true);
 
 		Assert.assertArrayEquals(new int [] {2, 3, 23}, ao.findPrimesInFile(fio, filePath, mm));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_findPrimesInFileThatDoesNotExist_Mocking() {
+		FileIO fio = mock(FileIO.class);
+		MyMath mm = mock(MyMath.class);
+		
+		String filePath = "src/test/resources/this_does_not_exist.txt";
+
+		when(fio.readFile(filePath)).thenThrow(IllegalArgumentException.class);
+
+		ao.findPrimesInFile(fio, filePath, mm);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_findPrimesInEmptyFile_Mocking() {
+		FileIO fio = mock(FileIO.class);
+		MyMath mm = mock(MyMath.class);
+		
+		String filePath = "src/test/resources/empty_file.txt";
+
+		when(fio.readFile(filePath)).thenThrow(IllegalArgumentException.class);
+
+		ao.findPrimesInFile(fio, filePath, mm);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_findPrimesInFileWithInvalidEntries_Mocking() {
+		FileIO fio = mock(FileIO.class);
+		MyMath mm = mock(MyMath.class);
+		
+		String filePath = "src/test/resources/invalid_entries.txt";
+
+		when(fio.readFile(filePath)).thenReturn(new int [] {1, 2, 9});
+
+		when(mm.isPrime(1)).thenThrow(IllegalArgumentException.class);
+		when(mm.isPrime(2)).thenReturn(true);
+		when(mm.isPrime(9)).thenReturn(false);
+
+		ao.findPrimesInFile(fio, filePath, mm);
+	}
+
+	@Test
+	public void test_findPrimesInFileAllPrime_Mocking() {
+		FileIO fio = mock(FileIO.class);
+		MyMath mm = mock(MyMath.class);
+		
+		String filePath = "src/test/resources/all_prime.txt";
+
+		when(fio.readFile(filePath)).thenReturn(new int [] {2, 5, 83});
+
+		when(mm.isPrime(2)).thenReturn(true);
+		when(mm.isPrime(5)).thenReturn(true);
+		when(mm.isPrime(83)).thenReturn(true);
+
+		Assert.assertArrayEquals(new int [] {2, 5, 83}, ao.findPrimesInFile(fio, filePath, mm));
+	}
+
+	@Test
+	public void test_findPrimesInFileNonePrime_Mocking() {
+		FileIO fio = mock(FileIO.class);
+		MyMath mm = mock(MyMath.class);
+		
+		String filePath = "src/test/resources/none_prime.txt";
+
+		when(fio.readFile(filePath)).thenReturn(new int [] {4, 8, 27, 121});
+
+		when(mm.isPrime(4)).thenReturn(false);
+		when(mm.isPrime(8)).thenReturn(false);
+		when(mm.isPrime(27)).thenReturn(false);
+		when(mm.isPrime(121)).thenReturn(false);
+
+		Assert.assertArrayEquals(new int [] {}, ao.findPrimesInFile(fio, filePath, mm));
 	}
 
 }
